7b5e567f731f852987878500992990d15453ef26
Jan Stavěl @ RedHat <jstavel@redhat.com>
2017-05-12 18:28:33 +0200

    parse the whole Type test sentence.

--- src/rhsm/dbus/parser.clj
+++ src/rhsm/dbus/parser.clj
@@ -3,26 +3,41 @@
     rhsm.dbus.parser
   (:require  [instaparse.core :refer [defparser]]
              [clojure.core.match :refer [match]]
-             [rhsm.dbus.ebnf :as ebnf]))
+             [rhsm.dbus.ebnf :as ebnf]
+             [clojure.string :as str]))
 
-(defparser parse-type-signature
-  (str "<TYPE_SIGNATURE> = TYPE TYPE_SIGNATURE | EPSILON"
-       ebnf/type-signature-ebnf))
+(defparser one-type-and-rest-parser
+    (str "<PARTS> = TYPE REST; REST = #'.*';" ebnf/type-signature-ebnf))
+
+(defn parse-type-signature [string]
+  (loop [x string
+         accumulator ()]
+    (let [result (one-type-and-rest-parser x)]
+      (if (-> result type (= instaparse.gll.Failure))
+        [(reverse accumulator) x]
+        (let [[ts rest] result]
+          (recur  (-> rest second) (conj accumulator ts)))))))
 
 (defparser string-parser
-  (str "<DATA> = (STRING | EMPTY_STRING) <DELIM> REST" ebnf/simple-data-ebnf))
+  (str "<DATA> = (STRING_WITH_ESCAPES | STRING | EMPTY_STRING) REST" ebnf/simple-data-ebnf))
 
-(defparser escaped-string-parser
-  (str "<DATA> = STRING <DELIM> REST" ebnf/simple-data-ebnf))
+(defparser integer-parser
+  (str "<DATA> = INTEGER REST" ebnf/simple-data-ebnf))
 
 (defn parse-string [input]
   (let [[result rest] (string-parser input)]
-    (println "result:" result "rest:" rest)
     (let [[rest-label rest-value] rest]
-      (println "rest-label:" rest-label "rest-value:" rest-value)
       (match result
-             [:EMPTY_STRING] ["" rest-value]
-             [:STRING _] [(second result) rest-value]
+             [:EMPTY_STRING] ["" (.trim rest-value)]
+             [:STRING _] [(second result) (.trim rest-value)]
+             :else [nil nil]))))
+
+(defn parse-integer [input]
+  (let [[result rest] (integer-parser input)]
+    (let [[rest-label rest-value] rest]
+      (match result
+             [:INTEGER _] [(-> result second Integer/parseInt)
+                           (.trim rest-value)]
              :else [nil nil]))))
 
 ;; (list [:TYPE [:BASIC [:INTEGER]]]  [:TYPE [:BASIC [:INTEGER]]])
@@ -31,11 +46,16 @@
          accumulator []
          string string]
     (if-not type ;; the case: (list)
-      accumulator
+      [accumulator string]
       (let [[parsed-value next-string] (match type
-                                              [:TYPE [:BASIC [:STRING]]] (parse-string string)
+                                              [:TYPE [:BASIC [:STRING]]]  (parse-string string)
+                                              [:TYPE [:BASIC [:INTEGER]]] (parse-integer string)
                                               :else (list))]
         (recur rest (conj accumulator parsed-value) next-string)))))
+
+(defn parse [string]
+  (let [[ts data] (parse-type-signature string)]
+    (parse-data ts (.trim data))))
 
 ;https://start.fedoraproject.org/; (def object-type-parser
 ;;   (insta/parser object-type-ebnf))
