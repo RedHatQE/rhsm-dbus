71894e1c7e4b82c2f7a2a491c5d1616bab07007d
Jan Stavěl @ RedHat <jstavel@redhat.com>
2017-05-16 16:08:37 +0200

    updated version of the library

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject rhsm-dbus "0.1.0-SNAPSHOT"
+(defproject rhsm-dbus "0.9.0-SNAPSHOT"
   :description "DBUS Type System parser for clojure and java"
-  :url "http://example.com/FIXME"
+  :url ""
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
