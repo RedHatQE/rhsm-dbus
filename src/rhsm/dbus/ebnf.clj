980335f50eabc8b9eedfaa23874632ccf1d00f5a
Jan Stavěl @ RedHat <jstavel@redhat.com>
2017-07-07 15:35:47 +0200

    added a type 'array of hasmaps', added a boolean type

--- src/rhsm/dbus/ebnf.clj
+++ src/rhsm/dbus/ebnf.clj
@@ -2,24 +2,26 @@
 
 (def type-signature-ebnf
   "TYPE = ARRAY | BASIC | VAR;
-  BASIC = STRING | INTEGER;
+  BASIC = STRING | INTEGER | BOOLEAN;
   STRING = <'s'>;
   INTEGER = <'i'>;
+  BOOLEAN = <'b'>;
   VAR = <'v'>;
   ARRAY = <'a'> ARRAY_ITEM;
-  ARRAY_ITEM = BASIC | VAR | HASHMAP;
+  ARRAY_ITEM = BASIC | VAR | HASHMAP | ARRAY;
   HASHMAP = <'{'> KEY VALUE <'}'>;
   KEY = BASIC;
   VALUE = BASIC | VAR;
   DELIM = #' +';")
 
 (def simple-data-ebnf "
-  SIMPLE_ITEM = STRING |  INTEGER;
+  SIMPLE_ITEM = STRING | INTEGER | BOOLEAN;
   STRING = <DOUBLE_QUOTE> (ESCAPED_DOUBLE_QUOTE / NO_DOUBLE_QUOTE)* <DOUBLE_QUOTE>
   DOUBLE_QUOTE = '\"';
   NO_DOUBLE_QUOTE = #'[^\"]';
   ESCAPED_DOUBLE_QUOTE = BACKSLASH DOUBLE_QUOTE;
   BACKSLASH = '\\\\';
   INTEGER = #'[0-9]+';
+  BOOLEAN = 'false' | 'true';
   DELIM = #' +';
   REST = #'.*';")
